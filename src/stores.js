73917aa677d1b0b1eb0850f07f31077788151083
wolfex <91018450+wolfex01@users.noreply.github.com>
2022-04-13 19:52:00 +0200

    added store to store players

--- src/stores.js
+++ src/stores.js
@@ -1,1 +1,12 @@
 import { writable, readable } from 'svelte/store';
+// Store ;)
+export const players = writable([
+  {
+    name: 'John Doe',
+    id: 1,
+  },
+  {
+    name: 'Jane Doe',
+    id: 2,
+  },
+]);
--- src/views/List.svelte
+++ src/views/List.svelte
@@ -1,1 +1,10 @@
-<p>list</p>
+<script lang="ts">
+  import { players } from '../stores.js';
+</script>
+
+<div class="players">
+  {#each $players as player}
+    <p>{player.name}</p>
+    <p>{player.id}</p>
+  {/each}
+</div>
